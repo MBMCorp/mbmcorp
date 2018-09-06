8f23f43a9f3cbe9f38ae2e93c4b8b5d20d8184d9
ridhoemgl <ridhoemgl@gmail.com>
2018-09-06 18:12:38 +0700

    add usersModel.js

---
+++ server/controller/controllerDB.js
@@ -0,0 +1,62 @@
+const User = require('../models/usersModel')
+
+module.exports = {
+    createUser: (req, res) => {
+        User
+            .create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            })
+            .then(user => {
+                res.status(201).json({
+                    message: `create user success`,
+                    data: user
+                })
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: err.message
+                })
+            })
+    },
+    displayUser: (req, res) => {
+        // test display user on database
+        User
+            .find()
+            .then(user => {
+
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: err.message
+                })
+            })
+    },
+    updateUser: (req, res) => {
+        //  req.body checking condition here!!!
+        User
+            .update({
+                _id: id
+            },{
+                $set:{
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                    age: req.body.age,
+                    address: req.body.address
+                }
+            })
+            .then(code => {
+                res.status(200).json({
+                    message: 'Update data success',
+                    status: code
+                })
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: err.message
+                })
+            })
+    }
+}
---
+++ server/models/usersModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name:{
+        type: String
+    },
+    email:{
+        type: String
+    },
+    password:{
+        type: String
+    },
+    age:{
+        type: Number
+    },
+    address:{
+        type: String
+    },
+    photoLink:{
+        type: String
+    }
+},{
+    timestamps: true
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User

60ebf7c1508142c4c50286c0aa5e3263492475aa
sbachr <sbachr0501@gmail.com>
2018-09-06 20:45:22 +0700

    done with azure setting

--- server/app.js
+++ server/app.js
@@ -1,29 +1,34 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const RouterAzure = require('./routes/azure')
+
 
 const port = 3000;
-require('dotenv').config();
-const request = require('request');
-const mongoose   = require('mongoose');
+// require('dotenv').config();
+// const request = require('request');
+// const mongoose = require('mongoose');
 
-//Connecting do Mongoose
-const url = 'mongodb://localhost:27017/socmed-aggregator';
-mongoose.connect(url,{ useNewUrlParser: true });
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('We are connected');
-});
+// //Connecting do Mongoose
+// const url = 'mongodb://localhost:27017/socmed-aggregator';
+// mongoose.connect(url, { useNewUrlParser: true });
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+//   console.log('We are connected');
+// });
 
 app.use(cors());
 
-app.listen(port,()=>{
-  console.log(`application is on port:${port}`);
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const fbLogin = require('./routes/fbLogin');
-app.use('/fb-login',fbLogin);
+// const fbLogin = require('./routes/fbLogin');
+// app.use('/fb-login', fbLogin);
+
+app.use('/', RouterAzure)
+
+app.listen(port, () => {
+  console.log(`application is on port:${port}`);
+});

1071c69b85567aa3661a6b423764fc7821b69b0c
ridhoemgl <ridhoemgl@gmail.com>
2018-09-07 12:25:42 +0700

    Add API Music

---
+++ server/controller/controllerMusic.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+module.exports = {
+    findUserTrack: (req, res) => {
+        const options = {
+            url: `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${req.body.country}&api_key=${req.body.api_key}&format=json&limit=3`,
+        };
+        request.get(options, (error, response, tracks) => {
+            if (!error) {
+                res.status(200).json({
+                    data: JSON.parse(tracks)
+                })
+            } else {
+                res.status(500).json({
+                    msg: "error connecting to database"
+                })
+            }
+        })
+    }
+}

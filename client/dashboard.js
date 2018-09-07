a3036969f3d3e65bb1ce2a4e1841a15999d505d5
julianjca <julianjca11@gmail.com>
2018-09-07 14:39:03 +0700

    append list to client

--- client/dashboard.js
+++ client/dashboard.js
@@ -11,41 +11,15 @@ function showRec() {
 //Append List
 function showDB(movies) {
 
-  $(".recomendations").css("visibility", "visible");
   $(".example-equal-height").text("");
 
+  console.log("inilho", movies)
   movies.data.forEach(movie => {
-    $(".example-equal-height").append(
-      `<div class="example-equal-height-card">
-      <div class="example-equal-height-figure">
-          <i class="fa fa-music icon" aria-hidden="true"></i>
-      </div>
-      <div class="example-equal-height-caption">
-          <h5>Your Songs</h5>
-          <p>Churn rate non-disclosure agreement buyer focus ecosystem iPad.</p>
-      </div>
-    </div>
-    <div class="example-equal-height-card">
-      <div class="example-equal-height-figure">
-          <i class="fa fa-film icon" aria-hidden="true"></i>
-      </div>
-      <div class="example-equal-height-caption">
-          <h5>${movie.title}</h5>
-          <p>${movie.overview}</p><br>
-          <p><i class="fa fa-star-o" style="font-size:20px;color:yellow"></i>${movie.popularity}</p>
-      </div>
-    </div>
-    <div class="example-equal-height-card">
-      <div class="example-equal-height-figure">
-          <i class="fa fa-book icon" aria-hidden="true"></i>
-      </div>
-      <div class="example-equal-height-caption">
-          <h5>Your Books</h5>
-          <p>Twitter stock equity vesting period learning curve launch party pitch innovator series A financing
-              churn rate handshake.&nbsp;</p>
-      </div>
-    </div>`
-    )
+    $(".movieList").append(
+      `<h5>${movie.title}</h5>
+        <p>${movie.overview}</p><br>
+        <p><i class="fa fa-star-o" style="font-size:20px;color:yellow"></i>${movie.popularity}</p>
+        `)
   });
 
 }

f91346e47ef24875296d1f72993d9c4daa641a5a
julianjca <julianjca11@gmail.com>
2018-09-07 09:13:16 +0700

    finishing books api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { createUser,displayUser,findEmail } = require('../controller/controllerDB');
-const { movies } = require('../controller/controllerAPI');
+const { movies,books } = require('../controller/controllerAPI');
 
 router.post('/register',createUser);
 router.post('/login',displayUser);
 router.post('/movies',movies);
 router.get('/users',findEmail);
+router.get('/books',books);
 
 module.exports = router;
